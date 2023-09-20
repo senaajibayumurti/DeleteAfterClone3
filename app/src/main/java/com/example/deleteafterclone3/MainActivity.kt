245426538bcf9fd23cfa3c713764afcd08ba008d
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-09-20 08:54:39 +0700

    Created Spinner Provinsi and Countries

--- app/src/main/java/com/example/deleteafterclone3/MainActivity.kt
+++ app/src/main/java/com/example/deleteafterclone3/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.deleteafterclone3
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.deleteafterclone3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Membuat list untuk konten Spinner
+        val provinsi = arrayOf(
+            "Jawa Timur",
+            "Jawa Tengah",
+            "Jawa Barat",
+            "Jakarta",
+            "Yogyakarta"
+        )
+
+        //Memasukkan val provinsi ke spinner
+        with(binding){
+            val adapterProvinsi = ArrayAdapter<String>(this@MainActivity,
+            R.layout.simple_spinner_item, provinsi)
+            spinnerProvinsi.adapter = adapterProvinsi
+
+            //Memanggil dari string.xml
+            val countries = resources.getStringArray(com.example.deleteafterclone3.R.array.countries)
+            val adapterCountries = ArrayAdapter<String>(this@MainActivity,
+            R.layout.simple_spinner_item, countries)
+            spinnerCountry.adapter = adapterCountries
+        }
     }
 }
